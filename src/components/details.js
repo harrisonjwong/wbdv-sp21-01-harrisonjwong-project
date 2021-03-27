7a15ed58c824c2fefa9e904f618d16511255ec39
Harrison Wong <harrisonjwong@yahoo.com>
2021-03-27 18:31:38 -0400

    Add loader and markdown renderer to details

--- src/components/details.js
+++ src/components/details.js
@@ -1,14 +1,16 @@
 import React, {useEffect, useState} from 'react';
 import {useHistory, useParams} from 'react-router-dom';
 import redditService from '../services/reddit-service';
+import ReactMarkdown from 'react-markdown';
 
 const Details = () => {
   const {redditId} = useParams();
   const [threadDetails, setThreadDetails] = useState({});
+  const [loaded, setLoaded] = useState(false);
   const history = useHistory();
   useEffect(() => {
     redditService.findThreadById(redditId).then(resp => {
-      console.log(resp[0].data.children[0].data)
+      setLoaded(true);
       setThreadDetails(resp[0].data.children[0].data)
     })
   }, [redditId]);
@@ -18,38 +20,54 @@ const Details = () => {
   }
   return (
     <div>
-      <i className='fas fa-arrow-left fa-2x wbdv-clickable' onClick={() => history.goBack()}/>
-      <h3>{threadDetails.title}</h3>
-      <img className='mb-2' src={threadDetails.thumbnail} alt={threadDetails.title}/>
-      <ul className='list-group'>
-        <li className='list-group-item'>Date Posted: {transformDate(threadDetails.created_utc)}</li>
-        <li className='list-group-item'>
-          <a href={`https://www.reddit.com/r/${threadDetails.subreddit}`}>
-            Subreddit: {threadDetails.subreddit_name_prefixed}
-          </a>
-        </li>
-        <li className='list-group-item'>
-          <a href={threadDetails.url}>Content Link ({threadDetails.domain})</a>
-        </li>
-        <li className='list-group-item'>
-          <a href={`https://www.reddit.com${threadDetails.permalink}`}>Comments: {threadDetails.num_comments}</a>
-        </li>
-        <li className='list-group-item'>Upvotes: {threadDetails.ups} ({`${threadDetails.upvote_ratio * 100}%`})</li>
-        <li className='list-group-item'>Awards: {threadDetails.total_awards_received}</li>
-
-        {
-          threadDetails.post_hint === 'image' &&
+      {
+        !loaded &&
+        <div className="d-flex justify-content-center">
+          <div className="spinner-border m-5">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      }
+      {
+        loaded &&
+        <div>
+          <i className='fas fa-arrow-left fa-2x wbdv-clickable' onClick={() => history.goBack()}/>
+          <h3>{threadDetails.title}</h3>
+          {!threadDetails.is_self && <img className='mb-2' src={threadDetails.thumbnail} alt={threadDetails.title}/>}
+          <ul className='list-group'>
+            <li className='list-group-item'>Date Posted: {transformDate(threadDetails.created_utc)}</li>
             <li className='list-group-item'>
-              <img className='wbdv-image' src={threadDetails.url} alt={threadDetails.title}/>
+              <a href={`https://www.reddit.com/r/${threadDetails.subreddit}`} target='_blank' rel='noreferrer'>
+                Subreddit: {threadDetails.subreddit_name_prefixed}
+              </a>
             </li>
-        }
-        {
-          threadDetails.selftext &&
-          <li className='list-group-item'>
-            {threadDetails.selftext}
-          </li>
-        }
-      </ul>
+            <li className='list-group-item'>
+              <a href={threadDetails.url} target='_blank' rel='noreferrer'>
+                Content Link ({threadDetails.domain})
+              </a>
+            </li>
+            <li className='list-group-item'>
+              <a href={`https://www.reddit.com${threadDetails.permalink}`} target='_blank' rel='noreferrer'>
+                Comments: {threadDetails.num_comments}
+              </a>
+            </li>
+            <li className='list-group-item'>Upvotes: {threadDetails.ups} ({`${threadDetails.upvote_ratio * 100}%`})</li>
+            <li className='list-group-item'>Awards: {threadDetails.total_awards_received}</li>
+            {
+              threadDetails.post_hint === 'image' &&
+              <li className='list-group-item'>
+                <img className='wbdv-image' src={threadDetails.url} alt={threadDetails.title}/>
+              </li>
+            }
+            {
+              threadDetails.selftext &&
+              <li className='list-group-item'>
+                <ReactMarkdown source={threadDetails.selftext}/>
+              </li>
+            }
+          </ul>
+        </div>
+      }
     </div>
   );
 }
